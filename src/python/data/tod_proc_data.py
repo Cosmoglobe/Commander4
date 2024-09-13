6501d921e36e8b23059e7ca52f7637e093930323
Martin Reinecke <martin@mpa-garching.mpg.de>
2024-09-13 10:01:35 +0200

    new class

---
+++ src/python/data/tod_proc_data.py
@@ -0,0 +1,9 @@
+from .band import Band
+
+class TodProcData:
+    def __init__(self, inp: tuple[Band, ...])
+        self._data = inp
+
+    @property
+    def data(self) -> tuple[Band, ...]:
+        return self._data
