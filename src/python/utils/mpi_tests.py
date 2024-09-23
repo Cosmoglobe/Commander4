4a7769f21d51cdff14e79cd2dc6fdd10c662f1d8
Martin Reinecke <martin@mpa-garching.mpg.de>
2024-09-23 12:12:07 +0200

    add tests for in-place reductions and persistent collectives; can't test them locally

--- src/python/utils/mpi_tests.py
+++ src/python/utils/mpi_tests.py
@@ -16,8 +16,8 @@ def do_benchmark(comm, work, name, msg_size):
 
 def collectiveBench(comm, size):
     """Tests performance of collective communications of the given size
-       on the given communicator. Uses the buffer protocol-based mpi4py
-       scheme, i.e. requires array-like objects.
+    on the given communicator. Uses the buffer protocol-based mpi4py
+    scheme, i.e. requires array-like objects.
 
     Parameters
     ----------
@@ -42,10 +42,74 @@ def collectiveBench(comm, size):
     do_benchmark(comm, lambda : comm.Allreduce(buf, recvbuf, MPI.SUM), "Allreduce", size*2*(comm.size-1))
 
 
+def collectiveBenchInplace(comm, size):
+    """Tests performance of collective communications of the given size
+    on the given communicator. Uses the buffer protocol-based mpi4py
+    scheme, i.e. requires array-like objects.
+    Where possible, in-place communication is used.
+
+    Parameters
+    ----------
+    comm : MPI comunicator to use
+    size : float
+        message size in bytes
+    """
+    if comm.rank == 0 and size < 1e6:
+        print("Warning: small message size. You may not get good estimates for actual performance")
+
+    if comm.rank == 0:
+        print(f"Testing buffer-based collective communications on {comm.size} tasks with a message size of {size/1e9}GB")
+
+    # warming up
+    for i in range(10):
+        comm.Barrier()
+    do_benchmark(comm, lambda : comm.Barrier(), "Barrier", 0)
+    buf = np.ones(int(size/8), dtype=np.float64)
+    do_benchmark(comm, lambda : comm.Bcast(buf, 0), "Bcast", size*(comm.size-1))
+    do_benchmark(comm, lambda : comm.Reduce(MPI.IN_PLACE, buf, MPI.SUM, 0), "Reduce", size*(comm.size-1))
+    do_benchmark(comm, lambda : comm.Allreduce(MPI.IN_PLACE, buf, MPI.SUM), "Allreduce", size*2*(comm.size-1))
+
+
+def collectiveBenchPersistent(comm, size):
+    """Tests performance of collective communications of the given size
+    on the given communicator. Uses the buffer protocol-based mpi4py
+    scheme, i.e. requires array-like objects.
+    This routine uses persistent collective, a feature introcuced in the
+    MPI 4 standard. It might not work wir most current MPI libraries.
+
+    Parameters
+    ----------
+    comm : MPI comunicator to use
+    size : float
+        message size in bytes
+    """
+    if comm.rank == 0 and size < 1e6:
+        print("Warning: small message size. You may not get good estimates for actual performance")
+
+    if comm.rank == 0:
+        print(f"Testing buffer-based collective communications on {comm.size} tasks with a message size of {size/1e9}GB")
+
+    # warming up
+    for i in range(10):
+        comm.Barrier()
+    do_benchmark(comm, lambda : comm.Barrier(), "Barrier", 0)
+    buf = np.ones(int(size/8), dtype=np.float64)
+    recvbuf = buf.copy()
+    req = comm.Bcast_init(buf,0)
+    do_benchmark(comm, lambda : req.Start(), "Bcast", size*(comm.size-1))
+    del req
+    req = comm.Reduce_init(buf, recvbuf, MPI.SUM, 0)
+    do_benchmark(comm, lambda : req.Start(), "Reduce", size*(comm.size-1))
+    del req
+    req = comm.Allreduce_init(buf, recvbuf, MPI.SUM)
+    do_benchmark(comm, lambda : req.Start(), "Allreduce", size*2*(comm.size-1))
+    del req
+
+
 def collectiveBenchSimple(comm, size):
     """Tests performance of collective communications of the given size
-       on the given communicator. Uses the "simple" mpi4py scheme, i.e. without
-       relying on the buffer protocol.
+    on the given communicator. Uses the "simple" mpi4py scheme, i.e. without
+    relying on the buffer protocol.
 
     Parameters
     ----------
@@ -78,3 +142,5 @@ args = parser.parse_args()
 
 collectiveBench(MPI.COMM_WORLD, args.message_size)
 collectiveBenchSimple(MPI.COMM_WORLD, args.message_size)
+#collectiveBenchInplace(MPI.COMM_WORLD, args.message_size)
+#collectiveBenchPersistent(MPI.COMM_WORLD, args.message_size)
