0b2af68733b30f941d354ee87708b06065ed6851
Eirik Gjerløw <eirik.gjerlow@astro.uio.no>
2025-02-14 08:31:01 +0100

    Add output.py to deal with logging

---
+++ src/python/output.py
@@ -0,0 +1,73 @@
+import logging
+import logging.config
+
+
+def init_loggers(logger_params): 
+    """
+    Intended usage: This function is called once at the very beginning of the
+    program; after that, any function can call
+    logger = logging.getLogger(__name__) to get a fully configured logger. This
+    logger has debug(), info(), warning(), error() and critical() logging
+    functions, to be used as appropriate.
+
+    Input arguments:
+        logger_params (pixell.bunch): The part of the parameters that deal with
+            logging. Each entry configures a separate logger, and currently,
+            the two entry names supported are
+                console: Needs a 'level' entry which can be one of the five
+                    logging levels (in lower or upper case)
+                file: Needs a 'level' parameter like console, as well as a
+                    'filename' entry, giving the output file name.
+    """
+    config_dict = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'style': '{',
+                'format': '{asctime} - {name} - {levelname} - {message}'
+            },
+        },
+        'handlers': {},
+        'loggers': {
+            None: { # Here we can potentially add more loggers, with custom names.
+                'handlers': [],
+                'level': 'DEBUG',
+                'propagate': True
+            }
+        }
+    }
+    if 'console' in logger_params:
+        config_dict['handlers']['console'] = {
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+            'level': logger_params.console.level.upper()
+        }
+        config_dict['loggers'][None]['handlers'].append('console')
+    if 'file' in logger_params:
+        config_dict['handlers']['file'] = {
+            'class': 'logging.FileHandler',
+            'formatter': 'standard',
+            'level': logger_params.file.level.upper(),
+            'filename': logger_params.file.filename,
+            'mode': 'a'
+        }
+        config_dict['loggers'][None]['handlers'].append('file')
+    logging.config.dictConfig(config_dict)
+
+
+def logassert(assertion, errmsg, logger):
+    try:
+        assert assertion
+    except AssertionError as err:
+        logger.exception(errmsg)
+        raise err
+
+
+def logassert_np(assertion, errmsg, logger):
+    from numpy.testing import assert_ as myassert
+    try:
+        myassert(assertion, '')
+    except AssertionError as err:
+        logger.exception(errmsg)
+        raise err
