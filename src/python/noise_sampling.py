a2355be73eec45b4c947d37cdf799d1d5467d7a8
jgslunde <jonas.s.lunde@outlook.com>
2025-07-23 21:25:44 +0200

    Fixed type hints and comments

--- src/python/noise_sampling.py
+++ src/python/noise_sampling.py
@@ -2,10 +2,10 @@ import numpy as np
 from scipy.fft import rfft, irfft, rfftfreq
 from pixell import utils
 from numpy.typing import NDArray
+import logging
 
-
-def _inversion_sampler_1d(lnL, grid_points):
-    """ Performs 1D inversion sampling on a grid. This involves calculating the cummulative log-likelihood, normalizing
+def _inversion_sampler_1d(lnL: NDArray, grid_points: NDArray) -> float:
+    """ Performs 1D inversion sampling on a grid. This involves calculating the cumulative log-likelihood, normalizing
         this to be contained in [0,1], drawing a random number in [0,1], and matching that to a (interpolated) grid point.
         Args:
             lnL (np.ndarray): Array of log-likelihood values at each grid point.
@@ -15,14 +15,14 @@ def _inversion_sampler_1d(lnL, grid_points):
     """
     lnL -= np.max(lnL)
     L = np.exp(lnL)  # Calculate the linear likelihood.
-    cdf = np.cumsum(L)  # Cummulative likelihood.
+    cdf = np.cumsum(L)  # Cumulative likelihood.
     cdf /= cdf[-1]  # Constrain it to [0,1].
     u = np.random.uniform(0, 1)
     sample = np.interp(u, cdf, grid_points)  # Find the x-value that matches the y-value we drew.
     return sample
 
 
-def sample_noise_PS_params(n_corr, sigma0, f_samp, alpha_start, freq_max=3.0, n_grid=100, n_burnin=5):
+def sample_noise_PS_params(n_corr: NDArray, sigma0: float, f_samp: float, alpha_start: float, freq_max=3.0, n_grid=100, n_burnin=5) -> tuple[float,float]:
     """ Function for drawing a sample of the fknee and alpha parameters for the correlated noise under the
         power spectrum data model PS = sigma0*(f/fknee)**alpha, where sigma0 is known.
         Note that this relates *only* to the correlated noise, without the "flat" white noise.
@@ -77,13 +77,13 @@ def sample_noise_PS_params(n_corr, sigma0, f_samp, alpha_start, freq_max=3.0, n_
 
 
 
-def corr_noise_realization_with_gaps(TOD, mask, sigma0, C_corr_inv, err_tol=1e-12, max_iter=100, rnd_seed=None):
+def corr_noise_realization_with_gaps(TOD: NDArray, mask: NDArray[np.bool_], sigma0: float, C_corr_inv: NDArray, err_tol=1e-12, max_iter=100, rnd_seed=None) -> NDArray:
     """ Draws a correlated noise realization given a TOD (with gaps/masked samples) a correlated noise power spectrum.
         Requires solving a CG, which this function solves in a very efficient way by splitting up the problem
         such that the CG only has to be performed on the missing data, not the full TOD (see arXiv:2011.06024).
         Args:
             TOD (np.array): The 1D time ordered data potentially containing gaps.
-            mask (np.array): A bollean array where False indices indicates missing or masked data.
+            mask (np.array): A boolean array where False indices indicates missing or masked data.
             sigma0 (float): The stationary white noise level of the data.
             C_corr_inv (np.array): The inverse covariance of the correlated signal we want to sample.
             err_tol (float): The error tolerance for the CG search.
@@ -92,6 +92,7 @@ def corr_noise_realization_with_gaps(TOD, mask, sigma0, C_corr_inv, err_tol=1e-1
         Returns:
             x_final (np.array): The TOD realization of the correlated noise.
     """
+    logger = logging.getLogger(__name__)
     def apply_filter(vec, Fourier_filter):
         return irfft(rfft(vec) * Fourier_filter, n=len(vec))
 
@@ -127,9 +128,9 @@ def corr_noise_realization_with_gaps(TOD, mask, sigma0, C_corr_inv, err_tol=1e-1
             else:
                 break
         if i == max_iter:
-            print(f"Corr noise CG failed to converge after {max_iter} iterations. Residual = {CG_solver.err} (err tol = {err_tol:.2e})")
+            logger.warning(f"Corr noise CG failed to converge after {max_iter} iterations. Residual = {CG_solver.err} (err tol = {err_tol:.2e})")
         else:
-            # print(f"Corr noise CG converged after {i} iterations. Residual = {CG_solver.err} (err tol = {err_tol:.2e})")
+            # logger.info(f"Corr noise CG converged after {i} iterations. Residual = {CG_solver.err} (err tol = {err_tol:.2e})")
             pass
         x_small = CG_solver.x
     else:
@@ -145,7 +146,7 @@ def corr_noise_realization_with_gaps(TOD, mask, sigma0, C_corr_inv, err_tol=1e-1
 
 
 
-def inefficient_corr_noise_realization_with_gaps(TOD: NDArray, mask: NDArray[np.bool_], sigma0: float, C_corr_inv: NDArray, err_tol=1e-12, max_iter=300, rnd_seed=None):
+def inefficient_corr_noise_realization_with_gaps(TOD: NDArray, mask: NDArray[np.bool_], sigma0: float, C_corr_inv: NDArray, err_tol=1e-12, max_iter=300, rnd_seed=None) -> NDArray:
     """ A simpler and less efficient implementation of the function 'corr_noise_realization_with_gaps'.
         This function performs the 'full' straight-forward CG search. Should only be used to test the proper function.
     """
