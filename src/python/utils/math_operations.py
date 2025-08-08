61656335ea82a0ad71158a14488b9a98f3b88241
Martin Reinecke <martin@mpa-garching.mpg.de>
2025-08-08 09:49:52 +0200

    small tweaks; add pseudo inverse alm2map

--- src/python/utils/math_operations.py
+++ src/python/utils/math_operations.py
@@ -11,14 +11,14 @@ import ducc0
 
 
 def nalm(lmax: int, mmax: int) -> int:
-    """ Calculates the number of a_lm elements for a spherical harmonic representation up to l<=lmax and m<=mmax.
+    """Calculates the number of a_lm elements for a spherical harmonic representation up to l<=lmax and m<=mmax.
     """
     return ((mmax+1)*(mmax+2))//2 + (mmax+1)*(lmax-mmax)
 
 
 # MR FIXME: I'm not absolutely sure that this is fully correct. Please double-check!
 def gaussian_random_alm(lmax, mmax, spin, ncomp):
-    """ Calculates Gaussianly distributed alms for the complex alm convension (not storing m<0 because map is real.)
+    """Calculates Gaussianly distributed alms for the complex alm convension (not storing m<0 because map is real.)
     """
     res = np.random.normal(0., 1., (ncomp, nalm(lmax, mmax))) \
      + 1j*np.random.normal(0., 1., (ncomp, nalm(lmax, mmax)))
@@ -42,7 +42,7 @@ def _prep_input(arr_in, arr_out, nside, spin):
         if arr_out is not None:
             arr_out = arr_out.reshape((1,-1))
 
-    if arr_in.ndim !=2 or arr_out.ndim != 2:
+    if arr_in.ndim !=2 or (arr_out is not None and arr_out.ndim != 2):
         raise RuntimeError("bad array dimensionality") 
 
     if nside not in hp_geominfos:
@@ -67,6 +67,63 @@ def alm_to_map_adjoint(mp: NDArray, nside: int, lmax: int, *, spin: int=0,
     return out if ndim_in == 2 else out.reshape((-1,))
 
 
+def pseudo_alm_to_map_inverse(map: NDArray, nside: int, lmax: int, *, spin: int=0,
+               nthreads: int=1, out=None, epsilon: float, maxiter: int) -> NDArray:
+    """Tries to extract spherical harmonic coefficients from (sets of) one or two maps
+    by using the iterative LSMR algorithm.
+    
+    Parameters
+    ----------
+    map: numpy.ndarray(([ncomp,] 12*nside**2), dtype=numpy.float32 or numpy.float64
+    nside: int
+        nside parameter of the Healpix map
+    lmax: int >= 0
+        the maximum l moment of the transform (inclusive).
+    spin: int >= 0
+        the spin to use for the transform.
+        If spin==0, ncomp must be 1, otherwise 2
+    nthreads: int >= 0
+        the number of threads to use for the computation
+        if 0, use as many threads as there are hardware threads available on the system
+    out: None or numpy.ndarray([ncomp,] (lmax+1)*(lmax+2)//2),
+         dtype=numpy.complex of same precision as `map`)
+        the set of spherical harmonic coefficients.
+        if `None`, a new suitable array is allocated
+    epsilon: float > 0
+        the relative tolerance used as a stopping criterion
+    maxiter: int >= 0
+        the maximum number of iterations before stopping the algorithm
+    
+    Returns
+    -------
+    numpy.ndarray(([ncomp,] (lmax+1)*(lmax+2)//2), dtype=numpy.complex of same accuracy as `map`)
+        the set of spherical harmonic coefficients.
+        If `out` was supplied, this will be the same object
+    
+    int:
+        the reason for stopping the iteration
+        1: approximate solution to the equation system found
+        2: approximate least-squares solution found
+        3: condition number of the equation system too large
+        7: maximum number of iterations reached
+    
+    int:
+        the iteration count
+    
+    float:
+        the residual norm, divided by the norm of `map`
+    
+    float:
+        the quality of the least-squares solution
+    """
+    map, out, ndim_in = _prep_input(map, out, nside, spin)
+    res = ducc0.sht.pseudo_analysis(map=map, alm=out, lmax=lmax, spin=spin,
+                                    nthreads=nthreads, **hp_geominfos[nside],
+                                    epsilon=epsilon, maxiter=maxiter)
+    out = res[0] if ndim_in == 2 else res[0].reshape((-1,))
+    return (out, res[1], res[2], res[3], res[4])
+
+
 def spherical_beam_to_bl(fwhm: float, lmax: int) -> NDArray:
     # expects FWHM in units of arcmin
     fwhm = (fwhm*u.arcmin).to('rad').value
@@ -87,7 +144,7 @@ def alm_dot_product(alm1: NDArray, alm2: NDArray, lmax: int) -> NDArray:
 
 
 def alm_complex2real(alm: NDArray[np.complex128], lmax: int) -> NDArray[np.float64]:
-    """ Coverts from the complex convention of storing alms when the map is real, to the real convention.
+    """Converts from the complex convention of storing alms when the map is real, to the real convention.
         In the real convention, the all m modes are stored, but they are all stored as real values, not complex.
         Args:
             alm (np.array): Complex alm array of length ((lmax+1)*(lmax+2))/2.
@@ -101,7 +158,7 @@ def alm_complex2real(alm: NDArray[np.complex128], lmax: int) -> NDArray[np.float
 
 
 def alm_real2complex(x: NDArray[np.float64], lmax: int) -> NDArray[np.complex128]:
-    """ Coverts from the real convention of storing alms when the map is real, to the complex convention.
+    """Converts from the real convention of storing alms when the map is real, to the complex convention.
         In the complex convention, the only m>=0 is stored, but are stored as complex numbers (m=0 still always real).
         Args:
             x (np.array): Real alm array of length (lmax+1)^2.
