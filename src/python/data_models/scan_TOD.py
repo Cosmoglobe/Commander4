4434072402575a9d4b423380ac3e89f08f7d477d
jgslunde <jonas.s.lunde@outlook.com>
2025-08-15 23:38:42 +0200

    Updated ScanTOD setup, in line with the new masking logic (should have been done a few commits ago)

--- src/python/data_models/scan_TOD.py
+++ src/python/data_models/scan_TOD.py
@@ -1,46 +1,58 @@
 import numpy as np
 from output import log
 import logging
-from numpy.testing import assert_ as myassert
+from numpy.typing import NDArray
 
 class ScanTOD:
-    def __init__ (self, value, theta, phi, psi, startTime, scanID):
+    def __init__ (self, tod, pix, psi, startTime, scanID, nside, fsamp, orb_dir_vec):
         logger = logging.getLogger(__name__)
-        log.logassert_np(value.ndim==1, "'value' must be a 1D array", logger)
-        log.logassert_np(value.dtype in [np.float64,np.float32], "'value' dtype must be np.float64", logger)
-        log.logassert_np(theta.shape==value.shape and phi.shape==value.shape and psi.shape==value.shape,
-            f"shape mismatch between input arrays. Theta: {theta.shape}, phi: {phi.shape}, psi: {psi.shape}, value: {value.shape}", logger)
-        self._value = value
-        self._theta = theta
-        self._phi = phi
+        log.logassert_np(tod.ndim==1, "'value' must be a 1D array", logger)
+        log.logassert_np(tod.dtype in [np.float64,np.float32], "TOD dtype must be floating type,"
+                         f" is {tod.dtype}", logger)
+        log.logassert_np(np.max(pix) < 12*nside**2, f"Largest pixel index {np.max(pix)}"
+                         f"is too large for the specified NSIDE ({nside}).", logger)
+        log.logassert_np(orb_dir_vec.size == 3, "orb_dir_vec must be a vector of size 3.", logger)
+        self._tod = tod
+        self._pix = pix
         self._psi = psi
         self._startTime = startTime
         self._scanID = scanID
+        self._nside = nside
+        self._fsamp = fsamp
+        self._orb_dir_vec = orb_dir_vec
 
     @property
     def nsamples(self) -> int:
-        return self._value.shape[0]
+        return self._tod.shape[0]
 
     @property
     def startTime(self) -> float:
         return self.startTime
 
     @property
-    def data(self):
-        return self._value, self._theta, self._phi, self._psi
+    def tod(self) -> NDArray[np.floating]:
+        return self._tod
 
     @property
-    def scanID(self):
+    def pix(self) -> NDArray[np.integer]:
+        return self._pix
+
+    @property
+    def psi(self) -> NDArray[np.floating]:
+        return self._psi
+
+    @property
+    def scanID(self) -> int:
         return self._scanID
-
-    # @property
-    # def sky_subtracted_tod(self):
-    #     if self._component_map is None:
-    #         raise ValueError("sky_subtracted_tod property not set. Gibbs iter = {self._sky_subtracted_tod_Gibbs_iter}")
-    #     return self._component_map
-
-    # @sky_subtracted_tod.setter
-    # def sky_subtracted_tod(self, tod):
-    #     if not self._component_map is None:  # Temporarily disabled, might want to add back later.
-    #         raise ValueError("DiffuseComponent does not allow for overwriting already set component_map parameter.")
-    #     self._component_map = map
+    
+    @property
+    def nside(self) -> int:
+        return self._nside
+    
+    @property
+    def fsamp(self) -> float:
+        return self._fsamp
+    
+    @property
+    def orb_dir_vec(self) -> NDArray[np.floating]:
+        return self._orb_dir_vec
