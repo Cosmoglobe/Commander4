ec1f06d32c3d7c1bc844a1059e9d0f9aaa5c297d
jgslunde <jonas.s.lunde@outlook.com>
2025-08-20 15:42:41 +0200

    Added a script for checking performance difference between old and new Huffman decompression.

---
+++ aux/test_new_huffman_performance.py
@@ -0,0 +1,82 @@
+import numpy as np
+import cmdr4_support
+import time 
+import sys
+from tqdm import trange
+sys.path.append("/mn/stornext/d23/cmbco/jonas/c4_testing/Commander4/src/python/utils/")
+sys.path.append("/mn/stornext/d23/cmbco/jonas/c4_testing/Commander4/")
+from commander_tod import commander_tod
+from huffman import Huffman
+
+oids = []
+pids = []
+filenames = []
+with open("/mn/stornext/d16/cmbco/bp/mathew/test/filelist_30.txt") as infile:
+    infile.readline()
+    for line in infile:
+        pid, filename, _, _, _ = infile.readline().split()
+        pids.append(f"{int(pid):06d}")
+        filenames.append(filename[1:-1])
+        oids.append(filename.split(".")[0].split("_")[-1])
+pids = np.array(pids)
+oids = np.array(oids)
+
+psi_encoded_list = []
+pix_encoded_list = []
+hufftree_list = []
+huffsymb_list = []
+for i in trange(1000):
+    pid = pids[i]
+    oid = oids[i]
+    com_tod = commander_tod("/mn/stornext/d16/cmbco/bp/mathew/test/", "LFI")
+    com_tod.init_file("070", oid)
+    psi_encoded = com_tod.load_field(f"/{pid}/18M/psi/")[()]
+    pix_encoded = com_tod.load_field(f"/{pid}/18M/pix/")[()]
+    hufftree = com_tod.load_field(f"/{pid}/common/hufftree")[()]
+    huffsymb = com_tod.load_field(f"/{pid}/common/huffsymb")[()]
+    psi_encoded_list.append(psi_encoded)
+    pix_encoded_list.append(pix_encoded)
+    huffsymb_list.append(huffsymb)
+    hufftree_list.append(hufftree)
+
+
+sizes = []
+t0 = time.time()
+for i in trange(1000):
+    hufftree = hufftree_list[i]
+    huffsymb = huffsymb_list[i]
+    psi_encoded = psi_encoded_list[i]
+    pix_encoded = pix_encoded_list[i]
+    huff = Huffman(tree=hufftree, symb=huffsymb)
+    pix1 = huff.Decoder(pix_encoded, numba_decode=True)
+    psi1 = huff.Decoder(psi_encoded, numba_decode=True)
+    sizes.append(pix1.size)
+print(f"old version finished in {time.time()-t0:.2f}s.")
+
+# decompress using the new code
+comp0 = np.frombuffer(pix_encoded.tobytes(), dtype=np.uint8)
+t0 = time.time()
+for i in trange(1000):
+    hufftree = hufftree_list[i]
+    huffsymb = huffsymb_list[i]
+    psi_encoded = psi_encoded_list[i]
+    pix_encoded = pix_encoded_list[i]
+    huff = Huffman(tree=hufftree, symb=huffsymb)
+    symb0 = np.array(huff.symbols)
+    comp0 = np.frombuffer(pix_encoded, dtype=np.uint8)
+    ninternal = len(huff.left_nodes)
+    tree0 = np.empty(ninternal*2+1, dtype=np.int64)
+    tree0[0] = len(huff.symbols) + ninternal
+    tree0[1:ninternal+1] = huff.left_nodes
+    tree0[ninternal+1:] = huff.right_nodes
+    pix0 = np.zeros(sizes[i], dtype=np.int64)
+    pix0 = cmdr4_support.utils.huffman_decode(comp0, tree0, symb0, pix0)
+    pix0 = np.cumsum(pix0)
+    comp0 = np.frombuffer(psi_encoded, dtype=np.uint8)
+    psi0 = np.zeros(sizes[i], dtype=np.int64)
+    psi0 = cmdr4_support.utils.huffman_decode(comp0, tree0, symb0, psi0)
+    psi0 = np.cumsum(psi0)
+print(f"cmdr4_support version finished in {time.time()-t0:.2f}s.")
+
+print(np.sum(pix1-pix0))
+print(np.sum(psi1-psi0))
