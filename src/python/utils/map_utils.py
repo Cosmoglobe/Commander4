fc777123c0f48267858513afd6d2fe7b4f2ae66c
jgslunde <jonas.s.lunde@outlook.com>
2025-10-09 00:27:23 +0200

    some speedups to sky projection calculations

--- src/python/utils/map_utils.py
+++ src/python/utils/map_utils.py
@@ -5,8 +5,16 @@ from src.python.data_models.detector_TOD import DetectorTOD
 from numpy.typing import NDArray
 import ducc0
 import os
+from numba import njit
+
+T_CMB = 2.725 * 1e6  # CMB temperature in uK_CMB units.
+C = 299792458  # m/s (Speed of light)
+T_CMB_div_C = T_CMB / C
+# Precomputing the conversion factor from 1 uK_CMB to 1 uK_RJ
+uK_CMB_to_uK_RJ_dict = {}
 
 
+@njit(fastmath=True)
 def get_static_sky_TOD(det_compsep_map: NDArray[np.floating], pix: NDArray[np.integer], psi: NDArray[np.floating]) -> DetectorTOD:
     """ Projects the current sky-model at our band frequency (in uK_RJ, without gain) into the
         specified scan pointing. The sky model does not include the orbital dipole.
@@ -20,16 +28,13 @@ def get_s_orb_TOD(scan: ScanTOD, experiment: DetectorTOD, pix: NDArray[np.intege
                   nthreads:int = None) -> NDArray:
     # If nthreads is not set, put it to how many threads OMP has.
     nthreads = int(os.environ["OMP_NUM_THREADS"]) if nthreads is None else nthreads
-    T_CMB = 2.725 * 1e6  # CMB temperature in uK_CMB units.
-    C = 299792458  # m/s (Speed of light)
-    # Precomputing the conversion factor from 1 uK_CMB to 1 uK_RJ (note that this conversion is only valid for temperatures close to the CMB).
-    uK_CMB_to_uK_RJ = (1*pysm3_u.uK_CMB).to(pysm3_u.uK_RJ, equivalencies=pysm3_u.cmb_equivalencies(experiment.nu*pysm3_u.GHz)).value
-    
-    # theta, phi = hp.pix2ang(experiment.nside, pix)
-    # LOS_vec = hp.ang2vec(theta, phi).astype(np.float32)
+    if experiment.nu not in uK_CMB_to_uK_RJ_dict:
+        uK_CMB_to_uK_RJ_dict[experiment.nu] = (1*pysm3_u.uK_CMB).to(pysm3_u.uK_RJ,
+                        equivalencies=pysm3_u.cmb_equivalencies(experiment.nu*pysm3_u.GHz)).value
     geom = ducc0.healpix.Healpix_Base(experiment.nside, "RING")
     LOS_vec = geom.pix2vec(pix, nthreads=nthreads)
-    dot_product = np.sum(scan.orb_dir_vec * LOS_vec, axis=-1, dtype=np.float32)  # How much do the LOS and orbital velocity align?
-    s_orb = T_CMB * dot_product / C  # The orbital dipole in units of uK_CMB.
-    s_orb = s_orb * uK_CMB_to_uK_RJ  # Converting to uK_RJ units.
+    LOS_vec *= scan.orb_dir_vec
+    s_orb = np.sum(LOS_vec, axis=-1, dtype=np.float32)  # How much do the LOS and orbital velocity align?
+    s_orb *= T_CMB_div_C
+    s_orb *= uK_CMB_to_uK_RJ_dict[experiment.nu]  # Converting to uK_RJ units.
     return s_orb.astype(np.float32)
