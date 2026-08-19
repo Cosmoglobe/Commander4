a5d4de5beccb4e7afefcf740b9bd342e2e7b2a3c
jgslunde <jonas.s.lunde@outlook.com>
2026-08-19 15:28:09 +0200

    Deleted a bunch of unused data models

--- src/commander4/data_models_unused/detector_group.py
+++
@@ -1,7 +0,0 @@
-from .detector import Detector
-
-class DetectorGroup:
-    @property
-    def detectors(self) -> tuple[Detector, ...]:
-        """Returns the list of detectors in this group"""
-        raise NotImplementedError()
--- src/commander4/data_models_unused/detector_group_map.py
+++
@@ -1,7 +0,0 @@
-from .detector_map import DetectorMap
-
-class DetectorGroupMap:
-    @property
-    def detectors(self) -> tuple[DetectorMap, ...]:
-        """Returns the list of detector maps in this group"""
-        raise NotImplementedError()
