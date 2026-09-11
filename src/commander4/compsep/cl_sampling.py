5987915b72881530d646abe8b6336c01d4c89fb2
Hidde Jense <JenseH@cardiff.ac.uk>
2026-09-11 14:12:25 +0200

    Flake8.

--- src/commander4/compsep/cl_sampling.py
+++ src/commander4/compsep/cl_sampling.py
@@ -4,15 +4,14 @@ import logging
 import numpy as np
 from numpy.typing import NDArray
 
-from mpi4py import MPI
-
 from commander4.sky.comp_list import CompList
+from commander4.compsep.processing import ClSamplingGroupConfig
 
 logger = logging.getLogger(__name__)
 
 
 class ClSamplingGroup:
-    def __init__(self, config: "ClSamplingGroupConfig", comp_list: CompList):
+    def __init__(self, config: ClSamplingGroupConfig, comp_list: CompList):
         self.config = config
         self.comp_list = comp_list
 
@@ -27,12 +26,12 @@ class ClSamplingGroup:
             sigma_l = component.sigma_l
 
             # P(C_l | s) = C_l^(-(2l + 1) / 2) exp(- (2l + 1) sigma_l / 2 C_l)
-            l = np.arange(2, sigma_l.shape[1])
+            ell = np.arange(2, sigma_l.shape[1])
             cl = np.zeros_like(sigma_l)
-            
+
             for i in range(sigma_l.shape[0]):
-                x = np.random.chisquare(df=2.*l-1.)
-                cl[i,l] = (2. * l + 1.) * sigma_l[i, l] / x
+                x = np.random.chisquare(df=2.*ell-1.)
+                cl[i, ell] = (2. * ell + 1.) * sigma_l[i, ell] / x
             proposal[group] = cl
 
         return proposal
